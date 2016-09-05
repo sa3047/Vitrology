5454621a723ccb93dc1343b372b295159f9856a7
sa3047 <swapnil_aher@yahoo.co.in>
2016-09-05 12:19:07 -0400

    Introduce Dashboard feature

---
+++ VitrologyWebApp_V1/app/controllers/main.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+/**
+ * @ngdoc function
+ * @name vitrologyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vitrologyApp
+ * Create user service which will give us registered users
+ */
+angular.module('vitrologyApp')
+  .controller('MainCtrl', function ($scope, $rootScope,$location, UserService) {
+      $scope.header = 'Registered users.';
+      $rootScope.hideMenus = true;
+
+      UserService.getAllUser(function(response){
+          var parseResponse = JSON.parse(response);
+          console.log('Users ' + parseResponse);
+
+          if (parseResponse) {
+              $scope.users = parseResponse;
+          }
+          
+      });
+
+      $scope.deleteUser = function (Id) {
+          console.log('Delete customer Id ' + Id);
+          UserService.deleteUser(Id, function (response) {
+              var parseResponse = JSON.parse(response);
+              console.log('Deleted user ' + parseResponse);
+
+              if (parseResponse) {
+                  console.log('Refreshing main page');
+
+                  UserService.getAllUser(function (result) {
+                      var parseUsers = JSON.parse(result);
+                      console.log('Users ' + parseUsers);
+
+                      if (parseUsers) {
+                          $scope.users = parseUsers;
+                      }
+
+                  });
+              }
+
+          });
+
+      };
+
+  });
