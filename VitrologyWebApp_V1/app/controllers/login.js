5ea0ca12ceb131c600079fb46caf8e401216f9d5
sa3047 <swapnil_aher@yahoo.co.in>
2016-09-09 17:43:21 -0400

    Fix display error on login page.

--- VitrologyWebApp_V1/app/controllers/login.js
+++ VitrologyWebApp_V1/app/controllers/login.js
@@ -10,7 +10,8 @@ angular.module('vitrologyApp')
     .controller('LoginCtrl', function ($scope, $rootScope, $location, AuthenticationService) {
         $scope.header = 'Login page.';
         $rootScope.hideMenus = false;
-
+        $scope.error = false;
+        $scope.errorMessage = '';
         // reset login status
         AuthenticationService.ClearCredentials();
 
@@ -18,16 +19,21 @@ angular.module('vitrologyApp')
             $scope.dataLoading = true;
             AuthenticationService.Login($scope.email, $scope.password, function (response) {
                 var parseResponse = JSON.parse(response);
-                console.log('Authenticated user ' + parseResponse);
-                if (parseResponse) {
+                console.log('Authenticated user ' + parseResponse.Success);
+                //console.log('Authenticated Message ' + parseResponse.Message);
+                if (parseResponse.Success) {
                     console.log('user id: ' + parseResponse.Id);
                     AuthenticationService.SetCredentials($scope.email, $scope.password, parseResponse.Id);
                     // TODO : Hide menu
                     $rootScope.hideMenus = true;
                     // Go to main page
+                    $scope.error = false;
+
                     $location.path('/');
                 } else {
-                    $scope.error = response.message;
+                    console.log('Authenticated Message: ' + parseResponse.Message);
+                    $scope.error = true;
+                    $scope.errorMessage = parseResponse.Message;
                     $scope.dataLoading = false;
                     $rootScope.hideMenus = false;
                 }
